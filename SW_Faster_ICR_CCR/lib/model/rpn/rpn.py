689eee87b0479425ef3a6ab91f348ce52a8fee2c
FengJunJian <446187960@qq.com>
2022-04-13 21:51:53 +0800

    add initilization with normal for RPN branch

--- SW_Faster_ICR_CCR/lib/model/rpn/rpn.py
+++ SW_Faster_ICR_CCR/lib/model/rpn/rpn.py
@@ -24,19 +24,19 @@ class _RPN(nn.Module):
         self.feat_stride = cfg.FEAT_STRIDE[0]
 
         # define the convrelu layers processing input feature map
-        self.RPN_Conv = nn.Conv2d(self.din, 512, 3, 1, 1, bias=True)
+        self.RPN_Conv = nn.Conv2d(self.din, 512, 3, 1, 1, bias=True)#Conv
 
         # define bg/fg classifcation score layer
         self.nc_score_out = (
             len(self.anchor_scales) * len(self.anchor_ratios) * 2
         )  # 2(bg/fg) * 9 (anchors)
-        self.RPN_cls_score = nn.Conv2d(512, self.nc_score_out, 1, 1, 0)
+        self.RPN_cls_score = nn.Conv2d(512, self.nc_score_out, 1, 1, 0)#RPN Cls
 
         # define anchor box offset prediction layer
         self.nc_bbox_out = (
             len(self.anchor_scales) * len(self.anchor_ratios) * 4
         )  # 4(coords) * 9 (anchors)
-        self.RPN_bbox_pred = nn.Conv2d(512, self.nc_bbox_out, 1, 1, 0)
+        self.RPN_bbox_pred = nn.Conv2d(512, self.nc_bbox_out, 1, 1, 0)#RPN Reg
 
         # define proposal layer
         self.RPN_proposal = _ProposalLayer(
@@ -50,7 +50,9 @@ class _RPN(nn.Module):
 
         self.rpn_loss_cls = 0
         self.rpn_loss_box = 0
-
+        for l in [self.RPN_Conv, self.RPN_cls_score, self.RPN_bbox_pred]:#***Init*** Important
+            torch.nn.init.normal_(l.weight, std=0.01)
+            torch.nn.init.constant_(l.bias, 0)
     @staticmethod
     def reshape(x, d):
         input_shape = x.size()
@@ -71,9 +73,9 @@ class _RPN(nn.Module):
         # get rpn classification score
         rpn_cls_score = self.RPN_cls_score(rpn_conv1)
 
-        rpn_cls_score_reshape = self.reshape(rpn_cls_score, 2)
-        rpn_cls_prob_reshape = F.softmax(rpn_cls_score_reshape, 1)
-        rpn_cls_prob = self.reshape(rpn_cls_prob_reshape, self.nc_score_out)
+        rpn_cls_score_reshape = self.reshape(rpn_cls_score, 2)#(1,30,38,67)
+        rpn_cls_prob_reshape = F.softmax(rpn_cls_score_reshape, 1)#(1,2,570,67)
+        rpn_cls_prob = self.reshape(rpn_cls_prob_reshape, self.nc_score_out)#(1,30,38,67)
 
         # get rpn offsets to the anchor boxes
         rpn_bbox_pred = self.RPN_bbox_pred(rpn_conv1)
@@ -82,7 +84,7 @@ class _RPN(nn.Module):
         cfg_key = "TRAIN" if self.training else "TEST"
 
         rois = self.RPN_proposal(
-            (rpn_cls_prob.data, rpn_bbox_pred.data, im_info, cfg_key)
+            (rpn_cls_prob, rpn_bbox_pred, im_info, cfg_key)
         )
 
         self.rpn_loss_cls = 0
